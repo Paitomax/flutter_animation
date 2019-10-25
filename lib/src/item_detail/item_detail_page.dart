fb609cfcfa9f8f9633c6ab2a4411a648600cab9c
Guilherme Nascimento <paitomax@gmail.com>
2019-10-25 20:36:18 -0300

    Added Rounded AppBar

--- lib/src/item_detail/item_detail_page.dart
+++ lib/src/item_detail/item_detail_page.dart
@@ -17,10 +17,44 @@ class ItemDetailPageState extends State<ItemDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.item.title),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: widget.item.backgroundColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(24),
+                ),
+              ),
+              expandedHeight: MediaQuery.of(context).size.height * 0.8,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  collapseMode: CollapseMode.pin,
+                  centerTitle: true,
+                  title: Text(widget.item.title,
+                      style: TextStyle(
+                        color: widget.item.textColor,
+                        fontSize: 16.0,
+                      )),
+                  background: Hero(
+                      tag: widget.item.title,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: widget.item.backgroundColor,
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(24),
+                              bottomRight: Radius.circular(24),
+                            )),
+                      ))),
+            ),
+          ];
+        },
+        body: Center(
+          child: Text("Sei de nada"),
+        ),
       ),
-      body: Hero(tag: widget.item.title, child: Container(color: widget.item.backgroundColor,)),
     );
   }
 }
